1ae6037a56cdbbd624adc160533f3037804a34a7
Guldariya Kurbonova <101057112+Guldariya-Kurbonova@users.noreply.github.com>
2025-05-18 11:36:27 +0500

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -10,3 +10,44 @@ select
 from employeeUni as u
 right join employees as e
 on u.id = e.id; 
+
+
+-- 1068. Product Sales Analysis I
+select  product_name,
+    year,
+    price
+from sales as s
+inner join product as p
+on s.product_id = p.product_id;
+
+-- 1581. Customer Who Visited but Did Not Make Any Transactions
+SELECT customer_id,
+   count(v.visit_id) as count_no_trans
+FROM visits as v
+where v.visit_id not in 
+(select visit_id from transactions)
+group by customer_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
