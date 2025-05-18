1b9e3cab358a85fbd8205e9b645ea6e1675a4741
Guldariya Kurbonova <101057112+Guldariya-Kurbonova@users.noreply.github.com>
2025-05-18 11:13:36 +0500

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,1 +1,12 @@
+--- BASIC JOINS
+--  1378. Replace Employee ID With The Unique Identifier  
+/*Write a solution to show the unique ID of each user, 
+  If a user does not have a unique ID replace just show null.
+  Return the result table in any order.*/
 
+select 
+    u.unique_id,
+    e.name
+from employeeUni as u
+right join employees as e
+on u.id = e.id; 
